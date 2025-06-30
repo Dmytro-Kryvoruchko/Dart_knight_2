46d92f04404eb709bba47c060fcd5d2f7c087be8
Dmytro-Kryvoruchko <dkrivoruchko79@gmail.com>
2025-06-30 22:46:03 +0300

    task_1

---
+++ dart_collections_journey/lib/main.dart
@@ -0,0 +1,30 @@
+import 'dart:math';
+
+void main() {
+  runTask1();
+}
+
+void runTask1() {
+  print('------------------- Task 1 -------------------');
+  final random = Random();
+  List<int> numbers = List.generate(100, (_) => random.nextInt(101));
+  print(numbers);
+
+  print('65-й елемент: ${numbers[64]}');
+
+  numbers.insert(49, 1000000000);
+
+  numbers.removeWhere((num) => [24, 45, 66, 88].contains(num));
+
+  int sum = 0;
+  for (var number in numbers) {
+    if (number % 3 == 0) sum += number;
+  }
+  print('Сума, кратна 3: $sum');
+
+  List<int> temp = [];
+  for (var number in numbers) {
+    if (number % 2 == 0) temp.add(number);
+  }
+  print('Кількість парних у temp: ${temp.length}');
+}
